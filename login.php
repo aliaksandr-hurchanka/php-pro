6cac53d6fb4e31c9410146601862b8ab4f9eedc5
Aliaksandr Hurchanka <aliaksandr_hurchanka@epam.com>
2017-02-16 22:34:16 +0300

    [C-9]: Add libs and plugins.

--- login.php
+++ login.php
@@ -13,9 +13,9 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="src/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="ui/src/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="src/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="ui/src/plugins/iCheck/square/blue.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -33,7 +33,7 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="ui/src/index.php" method="post">
       <div class="form-group has-feedback">
         <input type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
@@ -76,11 +76,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.2.3 -->
-<script src="src/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="ui/src/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="libs/bootstrap/js/bootstrap.min.js"></script>
 <!-- iCheck -->
-<script src="src/plugins/iCheck/icheck.min.js"></script>
+<script src="ui/src/plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
